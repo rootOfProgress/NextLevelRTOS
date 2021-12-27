b1196fd8a2faf4141a4ab37529ffdf684959c469
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2021-12-27 15:28:58 +0100

    feat: add vital files and documentation for bootup procedure

---
+++ os_kernel/src/main.rs
@@ -0,0 +1,17 @@
+//!
+//! Main crate for Kernelimplementation. Depends on lake_rtos_rt
+//! and the main function gets called after successful power on. 
+//!
+#![no_std]
+#![no_main]
+
+extern crate runtime;
+
+///
+/// Target function after hardware initialization, 
+/// acts as the first kernel function.
+/// 
+#[no_mangle]
+pub fn kernel_init() -> ! {
+    loop {}
+}
---
+++ runtime/src/lib.rs
@@ -0,0 +1,54 @@
+//!
+//! Contains the first entrypoint for startup procedure.
+//! Only for runtime purposes, acts as a platform for the kernel
+//! code build on top of it.
+//!
+
+#![no_std]
+
+use core::panic::PanicInfo;
+use core::ptr;
+
+///
+/// Mandatory resetfunction at adress 0x08000004.
+/// Gets called after power on the cpu.
+///
+#[no_mangle]
+pub unsafe extern "C" fn Reset() -> ! {
+
+    extern "C" {
+        static mut _sbss: u8;
+        static mut _ebss: u8;
+
+        static mut _sdata: u8;
+        static mut _edata: u8;
+        static _sidata: u8;
+    }
+
+    let count = &_ebss as *const u8 as usize - &_sbss as *const u8 as usize;
+    ptr::write_bytes(&mut _sbss as *mut u8, 0, count);
+
+    let count = &_edata as *const u8 as usize - &_sdata as *const u8 as usize;
+    ptr::copy_nonoverlapping(&_sidata as *const u8, &mut _sdata as *mut u8, count);
+
+    // reference to target function
+    extern "Rust" {
+        fn kernel_init() -> !;
+    }
+    
+    kernel_init();
+}
+
+///
+/// Manually create a section with points to the adress of 
+/// the reset function.
+///
+#[link_section = ".vector_table.reset_vector"]
+#[no_mangle]
+pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
