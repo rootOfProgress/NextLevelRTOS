f2af8b8e32e4f6362b2b2afcfd558540663fb668
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2022-04-16 17:57:20 +0200

    fix: rm unimplemented fn

--- kernel/src/proc/sched.rs
+++ kernel/src/proc/sched.rs
@@ -51,11 +51,10 @@ pub fn destroy() {
 
 // experimental
 pub fn sleep() {
-    let list_active = &mut *(RUNNABLE_TASKS as *mut List);
-    let list_sleeping = &mut *(SLEEPING_TASKS as *mut List);
-    if list.size > 1 {
-      // todo ...  
-    }
+    // let list_active = &mut *(RUNNABLE_TASKS as *mut List);
+    // let list_sleeping = &mut *(SLEEPING_TASKS as *mut List);
+    // if list.size > 1 {
+    // }
 }
 
 pub fn shift_task() -> u32 {
