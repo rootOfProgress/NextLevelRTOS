93147eb3f4fbf0c72f82bcc451c53c2ff365fbca
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2023-02-16 17:12:28 +0100

    feat: read out i2c

--- drohnentasks/src/position.c
+++ drohnentasks/src/position.c
@@ -4,24 +4,19 @@ readings_t position_readings;
 
 signed int complement(unsigned int payload, unsigned int pos, char axis)
 {
-    unsigned int result = 0;
+    signed int result = 0;
 
     // bit 9
-    signed int sign = (payload >> (pos - 1)) & ~(0xFFFE);
+    unsigned int sign = (payload >> (pos - 1)) & 1;
 
     // bit 8 .. 0
-    signed int value = (value) & ~(0xFE00);
+    signed int value = (value) & 0x1FF;
     // negative
-    if (sign == 1)
+    if (sign)
     {
         result = (((512 - value) + 1) * -1) / 256;
-        if (axis == 'y' && result == -1)
-            return 0;
-    }
-    else
-    {
-        if (axis == 'y' && ((value + 1) / 256) == 1)
-            return 0;
+        // if (axis == 'y' && result == -1)
+            // return 0;
     }
     return result;
 }
@@ -31,7 +26,7 @@ void calculate_position(void)
 {
     char payload[2] = {0x2D, 8};
     write(ADXL345, &payload, 2);
-    read(0x00, ADXL345, 1);
+    // read(0x00, ADXL345, 1);
 
     position_readings.x |= read(0x32, ADXL345, 1);
     position_readings.x |= (read(0x33, ADXL345, 1) << 8);
@@ -40,6 +35,7 @@ void calculate_position(void)
     position_readings.z |= read(0x36, ADXL345, 1);
     position_readings.z |= (read(0x37, ADXL345, 1) << 8);
 
+    // 10 bit resolution
     position_readings.x = complement(position_readings.x, 10, 'x');
     position_readings.y = complement(position_readings.y, 10, 'y');
     position_readings.z = complement(position_readings.z, 10, 'z');
