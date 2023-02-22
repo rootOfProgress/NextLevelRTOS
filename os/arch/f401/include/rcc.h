cbcad98c7eeee3df294b1c2d0e3ba7702a6335d1
Benjamin <bwilhelm@hyperstone.com>
2023-02-22 08:55:49 +0100

    wip: rcc clock

--- os/arch/f401/include/rcc.h
+++ os/arch/f401/include/rcc.h
@@ -4,11 +4,45 @@
 #define RCC_BASE 0x40023800
 #define I2C1EN 21
 
-// for stm32f4x...
-// #[repr(C)]
+typedef union rcc_cr
+{
+    struct
+    {
+        unsigned int hsion : 1, hsirdy : 1, : 1, hsitrim : 5, hsical : 8,
+                     hseon : 1, hserdy : 1, hsebyp : 1, csson : 1, : 4, pllon : 1, pllrdy : 1, plli2son : 1, plli2rdy : 1, : 4; 
+    } cr;
+    unsigned int raw;
+} rcc_cr_t;
+
+typedef union rcc_pllcfgr
+{
+    struct
+    {
+        unsigned int pllm0 : 1,
+                     pllm1 : 1,
+                     pllm2 : 1,   
+                     pllm3 : 1,   
+                     pllm4 : 1,   
+                     pllm5 : 1,
+                     plln  : 9,
+                           : 1,
+                     pllp0 : 1,
+                     pllp1 : 1,       
+                           : 4,
+                     pllsrc: 1,
+                           : 1,
+                     pllq0 : 1,
+                     pllq1 : 1,
+                     pllq2 : 1,
+                     pllq3 : 1,
+                           : 4;
+    } pllcfgr;
+    unsigned int raw;
+} rcc_pllcfgr_t;
+
 typedef struct Rcc {
-    unsigned int cr;
-    unsigned int pllcfgr;
+    rcc_cr_t cr;
+    rcc_pllcfgr_t pllcfgr;
     unsigned int cfgr;
     unsigned int cir; // 0x0c
     unsigned int ahb1rstr;
