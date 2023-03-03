c1ebe57c443201d9a3a95d43dc6dedbc7b4717b9
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2023-03-03 10:16:05 +0100

    feat: f401 spi

---
+++ packages/f401spi/src/gpio.c
@@ -0,0 +1,143 @@
+#include "gpio.h"
+#include "rcc.h"
+
+#define READ_REGISTER(addr)     (*(volatile unsigned int *) (addr))
+#define WRITE_REGISTER(addr, val) ((*(volatile unsigned int *) (addr)) = (unsigned int) (val))
+
+void init_gpio(GpioObject_t* gpio_object)
+{
+    RccRegisterMap_t* rcc_regs = (RccRegisterMap_t*) RCC_BASE;
+
+    switch (gpio_object->port)
+    {
+    case 'A':
+        WRITE_REGISTER(&rcc_regs->ahb1enr, READ_REGISTER(&rcc_regs->ahb1enr) | (1 << 0));
+        gpio_object->base_adress = (unsigned int*) GPIO_A_BASE;
+        break;
+    case 'B':
+        WRITE_REGISTER(&rcc_regs->ahb1enr, READ_REGISTER(&rcc_regs->ahb1enr) | (1 << 1));
+        gpio_object->base_adress = (unsigned int*) GPIO_B_BASE;
+        break;   
+    case 'C':
+        WRITE_REGISTER(&rcc_regs->ahb1enr, READ_REGISTER(&rcc_regs->ahb1enr) | (1 << 2));
+        gpio_object->base_adress = (unsigned int*) GPIO_C_BASE;
+        break;  
+    case 'D':
+        WRITE_REGISTER(&rcc_regs->ahb1enr, READ_REGISTER(&rcc_regs->ahb1enr) | (1 << 3));
+        gpio_object->base_adress = (unsigned int*) GPIO_D_BASE;
+        break;  
+    default:
+        break;
+    } 
+}
+
+void set_moder(GpioObject_t* t, ModerTypes_t moder)
+{
+    GpioRegisters_t* gpio_regs = get_registers(t);
+
+    switch (moder)
+    {
+    case InputMode:
+        WRITE_REGISTER(&gpio_regs->moder, READ_REGISTER(&gpio_regs->moder) | (INPUT << t->pin * 2));
+        break;
+    case GeneralPurposeOutputMode:
+        WRITE_REGISTER(&gpio_regs->moder, READ_REGISTER(&gpio_regs->moder) | (GENERALPURPOSEOUTPUT << t->pin * 2));
+        break;   
+    case AlternateFunctionMode:
+        WRITE_REGISTER(&gpio_regs->moder, READ_REGISTER(&gpio_regs->moder) | (ALTERNATE << t->pin * 2));
+        break;
+    case AnalogMode:
+        WRITE_REGISTER(&gpio_regs->moder, READ_REGISTER(&gpio_regs->moder) | (ANALOG << t->pin * 2));
+        break; 
+    default:
+        break;
+    }
+}
+
+GpioRegisters_t* get_registers(GpioObject_t* t)
+{
+    switch (t->port)
+    {
+    case 'A':
+        return (GpioRegisters_t*) ((unsigned int*) GPIO_A_BASE);
+    case 'B':
+        return (GpioRegisters_t*) ((unsigned int*) GPIO_B_BASE);
+    case 'C':
+        return (GpioRegisters_t*) ((unsigned int*) GPIO_C_BASE);
+    case 'D':
+        return (GpioRegisters_t*) ((unsigned int*) GPIO_D_BASE);
+    default:
+        // dummy
+        return (GpioRegisters_t*) ((unsigned int*) GPIO_A_BASE);
+    }
+}
+
+
+void into_af(GpioObject_t* t, unsigned int af_number)
+{
+    GpioRegisters_t* gpio_regs = get_registers(t);
+    if (t->pin < 8)
+    {
+        WRITE_REGISTER(&gpio_regs->afrl, READ_REGISTER(&gpio_regs->afrl) & ~(0xF << (t->pin * 4)));    
+        WRITE_REGISTER(&gpio_regs->afrl, READ_REGISTER(&gpio_regs->afrl) | (af_number << (t->pin * 4)));
+    }
+    else
+    {
+        // @todo: do not delete
+        
+        unsigned int pin  = t->pin - 8;
+        WRITE_REGISTER(&gpio_regs->afrh, READ_REGISTER(&gpio_regs->afrh) & ~(0xF << (pin * 4)));    
+        WRITE_REGISTER(&gpio_regs->afrh, READ_REGISTER(&gpio_regs->afrh) | (af_number << (pin * 4)));
+
+        // @todo: WARNING HARDCODED!
+        // WRITE_REGISTER(&gpio_regs->afrh, 0x00000770);
+
+    }
+}
+
+void set_speed(GpioObject_t* t, SpeedModes_t speed)
+{
+    GpioRegisters_t* gpio_regs = get_registers(t);
+    WRITE_REGISTER(&gpio_regs->ospeedr, READ_REGISTER(&gpio_regs->ospeedr) & ~(11 << (t->pin)));    
+    WRITE_REGISTER(&gpio_regs->ospeedr, READ_REGISTER(&gpio_regs->ospeedr) | (speed << (t->pin)));    
+}
+
+
+void set_pupdr(GpioObject_t* t, OutputTypes_t otype)
+{
+    GpioRegisters_t* gpio_regs = get_registers(t);
+    WRITE_REGISTER(&gpio_regs->pupdr, READ_REGISTER(&gpio_regs->pupdr) & ~(11 << (t->pin)));    
+    WRITE_REGISTER(&gpio_regs->pupdr, READ_REGISTER(&gpio_regs->pupdr) | (otype << (t->pin)));    
+}
+
+void set_otyper(GpioObject_t* t, OutputTypes_t otype)
+{
+    GpioRegisters_t* gpio_regs = get_registers(t);
+    WRITE_REGISTER(&gpio_regs->otyper, READ_REGISTER(&gpio_regs->otyper) & ~(otype << (t->pin)));
+    WRITE_REGISTER(&gpio_regs->otyper, READ_REGISTER(&gpio_regs->otyper) | (otype << (t->pin)));
+}
+
+void set_pin_on(GpioObject_t* gpio) 
+{
+    GpioRegisters_t* gpio_regs = get_registers(gpio);
+    WRITE_REGISTER((unsigned int*) &gpio_regs->odr, READ_REGISTER(&gpio_regs->odr) | (1 << gpio->pin));
+}
+void set_pin_off(GpioObject_t* gpio) 
+{
+    GpioRegisters_t* gpio_regs = get_registers(gpio);
+    WRITE_REGISTER((unsigned int*) &gpio_regs->odr, READ_REGISTER(&gpio_regs->odr) & ~(1 << gpio->pin));
+}
+
+unsigned int read_pin(GpioObject_t* gpio)
+{
+    GpioRegisters_t* gpio_regs = get_registers(gpio);
+    return READ_REGISTER((unsigned int*) &gpio_regs->idr) & (1 << gpio->pin);
+}
+
+
+void toggle_output_pin(GpioObject_t* t)
+{
+    GpioRegisters_t* gpio_regs = get_registers(t);
+
+    WRITE_REGISTER(&gpio_regs->odr, READ_REGISTER(&gpio_regs->odr) & ~(1 << t->port));
+}
---
+++ packages/f401spi/src/main.c
@@ -0,0 +1,150 @@
+#include "fifo.h"
+#include "gpio.h"
+#include "spi.h"
+#include "adxl345_common.h"
+#include "rcc.h"
+
+#define SV_YIELD_TASK __asm volatile ("mov r6, 2\n" \
+                                  "svc 0\n")
+
+#define SV_PRINT __asm volatile ("mov r6, 1\n" \
+                                  "svc 0\n")
+    
+
+#define READ_REGISTER(addr)     (*(volatile unsigned int *) (addr))
+#define WRITE_REGISTER(addr, val) ((*(volatile unsigned int *) (addr)) = (unsigned int) (val))
+
+
+void init_spi_gpio(GpioObject_t* obj)
+{
+    init_gpio(obj); //passt
+
+    // clk
+    obj->pin = 3;
+    obj->port = 'B';
+    set_moder(obj, AlternateFunctionMode);//passt
+    into_af(obj, 5);
+    set_otyper(obj, OpenDrain);
+    set_pupdr(obj, PullUp);
+    set_speed(obj, High);
+
+    // miso
+    obj->pin = 4;
+    obj->port = 'B';
+    // set_moder(obj, InputMode);
+    set_otyper(obj, PushPull);
+    set_pupdr(obj, PullUp);
+    set_speed(obj, High);
+    set_moder(obj, AlternateFunctionMode);
+    into_af(obj, 5);
+
+    // mosi
+    obj->pin = 5;
+    obj->port = 'B';
+    set_otyper(obj, OpenDrain);
+    set_pupdr(obj, PullUp);
+    set_speed(obj, High);
+    set_moder(obj, AlternateFunctionMode);
+    into_af(obj, 5);
+
+    // csl
+    obj->pin = 15;
+    obj->port = 'A';
+    set_pupdr(obj, PullUp);
+    set_moder(obj, AlternateFunctionMode);
+    into_af(obj, 5);
+
+    // set_otyper(obj, PushPull);
+    // set_pupdr(obj, PullUp);
+// 
+    // set_moder(obj, GeneralPurposeOutputMode);
+    // set_pin_on(obj);
+
+}
+
+void init_spi_peripherial()
+{
+    GpioObject_t t;
+    t.port = 'B';
+    init_spi_gpio(&t);
+    
+    RccRegisterMap_t* rcc_regs = (RccRegisterMap_t*) RccBaseAdress;
+    // WRITE_REGISTER(&rcc_regs->apb2rstr, READ_REGISTER(&rcc_regs->apb2enr) | (1 << SPI1EN));
+    // WRITE_REGISTER(&rcc_regs->apb2rstr, READ_REGISTER(&rcc_regs->apb2enr) & ~(1 << SPI1EN));
+
+    WRITE_REGISTER(&rcc_regs->apb2enr, READ_REGISTER(&rcc_regs->apb2enr) | (1 << SPI1EN));
+
+//    
+    SpiRegisterMap_t* spi_regs = (SpiRegisterMap_t*) Spi1BaseAdress;
+    WRITE_REGISTER(&spi_regs->cr1, READ_REGISTER(&spi_regs->cr1) & ~(1 << SPE));
+
+    WRITE_REGISTER(&spi_regs->cr1, READ_REGISTER(&spi_regs->cr1) | (1 << DFF) | (7 << 3) | (1 << MSTR) | (1 << SSM) | (1 << SSI));
+    WRITE_REGISTER(&spi_regs->cr1, READ_REGISTER(&spi_regs->cr1) | (1 << DFF) | (7 << 3) | (1 << MSTR));
+    // WRITE_REGISTER(&spi_regs->cr1, READ_REGISTER(&spi_regs->cr1) | (1 << SPE));
+
+    WRITE_REGISTER(&spi_regs->cr2, READ_REGISTER(&spi_regs->cr2) | (1 << SSOE));
+
+
+}
+
+void transfer(char data) 
+{
+    SpiRegisterMap_t* spi_regs = (SpiRegisterMap_t*) Spi1BaseAdress;
+    WRITE_REGISTER(&spi_regs->cr1, READ_REGISTER(&spi_regs->cr1) | (1 << SPE));
+
+    unsigned short tx_data = ((unsigned short) data) << 8; 
+    WRITE_REGISTER(&spi_regs->dr, tx_data);
+    while (!((READ_REGISTER(&(spi_regs)->sr) & (1 << BSY)) != 0) && !((READ_REGISTER(&(spi_regs)->sr) & (1 << RXNE)) != 1));
+    // while (!((READ_REGISTER(&(spi_regs)->sr) & (1 << TXE)) != 0) && !((READ_REGISTER(&(spi_regs)->sr) & (1 << BSY)) != 1));
+    // while (!((READ_REGISTER(&(spi_regs)->sr) & (1 << TXE)) != 0) && !((READ_REGISTER(&(spi_regs)->sr) & (1 << BSY)) != 1));
+    // WRITE_REGISTER(&spi_regs->dr, 0x2);
+    // while (!((READ_REGISTER(&(spi_regs)->sr) & (1 << RXNE)) != 0));
+
+    unsigned int foo = READ_REGISTER(&spi_regs->dr);
+}
+
+void __attribute((section(".main"))) __attribute__((__noipa__))  __attribute__((optimize("O0"))) main(void)
+{   
+
+    GpioObject_t t;
+    t.pin = 6;
+    t.port = 'B';
+    // set_pin_on(&t);
+
+    init_spi_peripherial();
+    transfer(0x3);
+    // READ_REGISTER(&spi_regs->dr);
+
+    // {
+    //     /* code */
+    
+    // set_pin_off(&t);
+    
+
+
+    // WRITE_REGISTER(&spi_regs->cr1, READ_REGISTER(&spi_regs->cr1) & ~(1 << SPE));
+
+    for (unsigned int i = 0; i < 2000; i++)
+    {}
+    // set_pin_on(&t);
+    
+    // set_pin_off(&t);
+    //     while (!((READ_REGISTER(&(spi_regs)->sr) & (1 << TXE)) != 0));
+    //     while (!((READ_REGISTER(&(spi_regs)->sr) & (1 << BSY)) != 1));
+    //     READ_REGISTER(&spi_regs->dr);
+
+    // }
+    // // READ_REGISTER(&spi_regs->dr);
+
+    // __asm("bkpt");
+// 
+    // set_pin_off(&t);
+    // set_pin_on(&t);
+
+    // while (1)
+    // {
+    //     SV_YIELD_TASK;
+    // }
+    
+
+}
