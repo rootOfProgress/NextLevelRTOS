4e39d16b136f4a8c4e61ad6969e47f076ba9ee8c
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2023-04-16 18:36:19 +0200

    f401_am2302: partly working

--- modules/addresses.h
+++ modules/addresses.h
@@ -2,6 +2,6 @@
 #define OS_HEADER_H
 #define allocate_addr 0x80019ad
 #define deallocate_addr 0x8001921
-#define create_task_addr 0x800288d
-#define timer_init_addr 0x8002af1
+#define create_task_addr 0x800286d
+#define timer_init_addr 0x8002abd
 #endif
--- os/include/externals.h
+++ os/include/externals.h
@@ -1,11 +1,9 @@
 #ifndef EXTERNALS_H
 #define EXTERNALS_H
 
-#define NUM_OF_EXTERNAL_FUNCTIONS 2
+#define NUM_OF_EXTERNAL_FUNCTIONS 0
 
-extern void template1(void);
-extern void template2(void);
 
-static void (*func_ptr[NUM_OF_EXTERNAL_FUNCTIONS])() = {template1,template2,};
+static void (*func_ptr[NUM_OF_EXTERNAL_FUNCTIONS])() = {};
 
 #endif
