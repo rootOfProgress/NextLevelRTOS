86425ca77752961efb1f10b1963037b8ec639886
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2023-04-16 18:30:24 +0200

    f401_am2302: partly working

--- packages/include/addresses.h
+++ packages/include/addresses.h
@@ -2,6 +2,6 @@
 #define OS_HEADER_H
 #define allocate_addr 0x80019ad
 #define deallocate_addr 0x8001921
-#define create_task_addr 0x80026d1
-#define timer_init_addr 0x8002935
+#define create_task_addr 0x800288d
+#define timer_init_addr 0x8002af1
 #endif
