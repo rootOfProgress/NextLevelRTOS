29f616f9aa076bd2502fd44bf6c72a2f3a80b85f
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2023-07-03 09:34:55 +0200

    feat: stm32 crc

--- os/arch/f401/src/rcc.c
+++ os/arch/f401/src/rcc.c
@@ -1,3 +1,11 @@
 #include "rcc.h"
+#include "lang.h"
 
 unsigned int RCC_BASE_ADRESS = 0x40023800;
+
+void activate_crc_clock(void)
+{
+    RccRegisterMap_t* rcc_regs = (RccRegisterMap_t*) RCC_BASE_ADRESS;
+
+    WRITE_REGISTER(&rcc_regs->ahb1enr, READ_REGISTER(&rcc_regs->ahb1enr) | (1 << 12));    
+}
