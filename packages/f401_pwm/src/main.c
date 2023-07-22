1a047a28d280139eedab36a978ba2ae9d058172f
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2023-07-22 21:20:50 +0200

    package(feat): pwm

---
+++ packages/f401_pwm/src/main.c
@@ -0,0 +1,18 @@
+#define SV_YIELD_TASK __asm volatile ("mov r6, 2\n" \
+                                  "svc 0\n")
+
+#define SV_PRINT __asm volatile ("mov r6, 1\n" \
+                                  "svc 0\n")
+
+#include "os_api.h"
+#include "rcc.h"
+#include "gpio.h"
+#include "uart.h"
+#include "tim2_5.h"
+
+int __attribute((section(".main"))) __attribute__((__noipa__))  __attribute__((optimize("O0"))) main(void)
+{
+    timer_pwm_init(2, 8000);
+    timer_start(2);
+    return 0;
+}
