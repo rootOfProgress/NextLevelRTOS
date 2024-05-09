c7b00cc044a68630652904aef3b93b0237dd899c
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2024-05-09 17:21:59 +0200

    other: some runime data updated

--- packages/include/addresses.h
+++ packages/include/addresses.h
@@ -2,16 +2,16 @@
 #define OS_HEADER_H
 #define allocate_addr 0x800188d
 #define deallocate_addr 0x8001801
-#define create_task_addr 0x80030ad
-#define timer_init_addr 0x80036c5
-#define task_sleep_addr 0x8002c5d
-#define create_task_addr 0x80030ad
-#define join_task_addr 0x80024e9
-#define memset_byte_addr 0x8001a89
+#define create_task_addr 0x8003161
+#define timer_init_addr 0x8003779
+#define task_sleep_addr 0x8002c8d
+#define create_task_addr 0x8003161
+#define join_task_addr 0x8002519
+#define memset_byte_addr 0x8001ab9
 #define link_exti_src_addr 0x8000fc5
 #define execute_priviledged_addr 0x8000c59
 #define link_adc_src_addr 0x80006b5
 #define lock_mutex_addr 0x8000161
 #define release_mutex_addr 0x800017b
-#define read_global_timer_addr 0x8002751
+#define read_global_timer_addr 0x8002781
 #endif
